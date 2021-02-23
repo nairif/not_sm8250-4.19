d8b968fd5d0fe1e5f2ed6306725403e25527c6f5
Vijayanand Jitta <vjitta@codeaurora.org>
2021-02-23 12:35:35 +0530

    ANDROID: dma-buf: Export is_dma_buf_file
    Export is_dma_buf_file function which will be used
    by the minidump module to get dmabuf info.
    
    Bug: 180978053
    Change-Id: Ic8f7dd4f0a620839ab19f52841e9a6541515133c
    Signed-off-by: Vijayanand Jitta <vjitta@codeaurora.org>
    Signed-off-by: //. <174224977+skye-tachyon@users.noreply.github.com>

--- drivers/dma-buf/dma-buf.c
+++ drivers/dma-buf/dma-buf.c
@@ -62,8 +62,6 @@ void __init init_dma_buf_kmem_pool(void)
 		SLAB_HWCACHE_ALIGN | SLAB_PANIC, NULL);
 }
 
-static inline int is_dma_buf_file(struct file *);
-
 struct dma_buf_list {
 	struct list_head head;
 	struct mutex lock;
@@ -541,10 +539,11 @@ static const struct file_operations dma_buf_fops = {
 /*
  * is_dma_buf_file - Check if struct file* is associated with dma_buf
  */
-static inline int is_dma_buf_file(struct file *file)
+int is_dma_buf_file(struct file *file)
 {
 	return file->f_op == &dma_buf_fops;
 }
+EXPORT_SYMBOL_GPL(is_dma_buf_file);
 
 static struct file *dma_buf_getfile(struct dma_buf *dmabuf, int flags)
 {
