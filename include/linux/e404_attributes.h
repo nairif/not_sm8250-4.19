ba585f5d411636ac3acbbc21d7ac52cf335157d4
kvsnr113 <kvsnrprojkt113@gmail.com>
2026-02-16 10:56:26 +0700

    e404_attr: introduce background app blocker
    Change-Id: I34c0aa73eacf586143e25f865c1efaa19988eea9
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>
    Signed-off-by: skye-tachyon <sonicgames772@gmail.com>

--- include/linux/e404_attributes.h
+++ include/linux/e404_attributes.h
@@ -6,11 +6,18 @@
 #include <linux/kernel.h>
 #include <linux/kobject.h>
 #include <linux/sysfs.h>
+#include <linux/sched.h>
+
+#define E404_BLOCKLIST_STRLEN 256
+#define E404_MAX_BLOCKED 16
+
+bool e404_comm_blocked(const char *comm);
 
 struct e404_attributes {
     int kgsl_skip_zeroing;
     int file_sync;
     int avoid_dirty_pte;
+    char bg_blocklist[E404_BLOCKLIST_STRLEN];
 };
 
 extern struct e404_attributes e404_data;
