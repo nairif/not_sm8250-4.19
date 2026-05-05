cfd0b0b2505839cda2ec1e1ef098ad595098f0e8
skye-tachyon <sonicgames772@gmail.com>
2026-05-05 16:19:09 -0300

    Revert "cpufreq: rename cpu_input_boost to cpu_event_boost"
    This reverts commit 206932fc9ab3f76bfd637e8b9e700cfb3202fdf4.
    
    Change-Id: I54451a7da6b07815ad2d97638f8fbff93e4cf5cb
    Signed-off-by: skye-tachyon <sonicgames772@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -96,7 +96,7 @@
 #include <linux/cpufreq_times.h>
 #include <linux/scs.h>
 #include <linux/devfreq_boost.h>
-#include <linux/cpu_event_boost.h>
+#include <linux/cpu_input_boost.h>
 
 #include <linux/pgtable.h>
 #include <asm/pgalloc.h>
@@ -2344,7 +2344,7 @@ long _do_fork(unsigned long clone_flags,
 	 */
 	if (task_is_zygote(current)) {
 		devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 50);
-		cpu_event_boost_kick_max(50);
+		cpu_input_boost_kick_max(50);
 	}
 
 	/*
